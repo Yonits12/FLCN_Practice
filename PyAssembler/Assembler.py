18e7f03934e02a52b4e31d2f510e3e7288012773
yonits12 <yonits12@gmail.com>
2020-04-26 09:44:38 +0300

    Working with BYTES instead of STRINGS

--- PyAssembler/Assembler.py
+++ PyAssembler/Assembler.py
@@ -3,6 +3,7 @@
 import sys
 import struct
 import argparse
+import binascii
 from enum import Enum
 
 
@@ -41,45 +42,50 @@ class Assembler:
 
 
     class AddressingMode(Enum):
-        REGISTER    = '00'
-        IMMEDIATE   = '01'
-        DIRECT      = '10'
-        INDIRECT    = '11'
+        REGISTER    = 0b00
+        IMMEDIATE   = 0b01
+        DIRECT      = 0b10
+        INDIRECT    = 0b11
 
 
     # Constants
     WORD_SIZE = 16
     ASCII_SIZE = 8
-    EMPTY_WORD = '0' * WORD_SIZE
+    EMPTY_WORD = 0b0
+    OPCODE_OFFSET = 12
+    REG1_OFFSET = 9
+    ADDR_MODE_OFFSET = 7
+    REG2_OFFSET = 4
+
     
     OPCODES = {
-        'mov': ("0000", 2),
-        'cmp': ("0001", 2),
-        'add': ("0010", 2),
-        'sub': ("0011", 2),
-        'not': ("0100", 1),
-        'clr': ("0101", 1),
-        'lea': ("0110", 2),
-        'inc': ("0111", 1),
-        'dec': ("1000", 1),
-        'jmp': ("1001", 1),
-        'jne': ("1010", 1),
-        'jz' : ("1011", 1),
-        'xor': ("1100", 2),
-        'or' : ("1101", 2),
-        'rol': ("1110", 2),
-        'nop': ("1111", 0)
+        'mov': (0b0000, 2),
+        'cmp': (0b0001, 2),
+        'add': (0b0010, 2),
+        'sub': (0b0011, 2),
+        'not': (0b0100, 1),
+        'clr': (0b0101, 1),
+        'lea': (0b0110, 2),
+        'inc': (0b0111, 1),
+        'dec': (0b1000, 1),
+        'jmp': (0b1001, 1),
+        'jne': (0b1010, 1),
+        'jz' : (0b1011, 1),
+        'xor': (0b1100, 2),
+        'or' : (0b1101, 2),
+        'rol': (0b1110, 2),
+        'nop': (0b1111, 0)
     }
 
     REGISTERS = {
-        'r0': "000",
-        'r1': "001",
-        'r2': "010",
-        'r3': "011",
-        'r4': "100",
-        'r5': "101",
-        'r6': "110",
-        'r7': "111"
+        'r0': 0b000,
+        'r1': 0b001,
+        'r2': 0b010,
+        'r3': 0b011,
+        'r4': 0b100,
+        'r5': 0b101,
+        'r6': 0b110,
+        'r7': 0b111
     }
 
 
@@ -87,6 +93,7 @@ class Assembler:
         self.sym_tbl = {}
         self.untranslated_exps = {}
         self.current_address = 0
+        self.current_line_idx = -1
 
 
     def convert_str_to_binary(self, data_string):
@@ -96,7 +103,7 @@ class Assembler:
         if '"' in data_string:
             raise ValueError("String must not include \" characters at .string line")
         self.current_address += (len(data_string) + 1)
-        return ''.join(format(ord(i), '08b')+'\n' for i in data_string+'\0').strip('\n')
+        return binascii.a2b_qp(data_string + '\0')
 
 
     def handle_addressing_mode(self, second_arg):
@@ -127,6 +134,23 @@ class Assembler:
         return asm_line
 
 
+    def section_handler(self, asm_line, translated_words):
+        section, *rest_line = asm_line.split(maxsplit=1)
+        
+        if section == Assembler.Sections.STRING.value:
+            translated_words[0] = self.convert_str_to_binary(rest_line[0])      # remove quotes
+        elif section == Assembler.Sections.DATA.value:
+            argument = rest_line[0].lstrip()
+            if argument.isnumeric():
+                self.current_address += 2
+                translated_words[0] = struct.pack('>H', int(argument))
+            else:
+                raise ValueError("The input was not a number at line: " + asm_line)
+        else:
+            return False
+        return True
+
+
     def handle_1_param_type(self, translated_words, arg):
         trans_word1, trans_word2 = translated_words
         
@@ -134,17 +158,17 @@ class Assembler:
         bin_reg = Assembler.REGISTERS.get(arg)
         
         if bin_reg:
-            trans_word1 = trans_word1[:4] + bin_reg + trans_word1[7:]
-        
-        elif arg.isnumeric():
-            trans_word2 = format(int(arg), '016b')
-        
+            trans_word1 = trans_word1 | bin_reg << Assembler.REG1_OFFSET
         else:
-            trans_word2 = arg
+            if arg.isnumeric():
+                trans_word2 = int(arg)
+            else:
+                self.untranslated_exps[self.current_line_idx] = arg
+                trans_word2 = None
+            self.current_address += 2
         
         translated_words[0] = trans_word1
-        translated_words[1] = '\n' + trans_word2 if trans_word2 != '' else trans_word2          # TODO it is terrible!
-
+        translated_words[1] = trans_word2
 
     def handle_2_param_type(self, translated_words, arg1, arg2):
         trans_word1, trans_word2 = translated_words
@@ -152,25 +176,25 @@ class Assembler:
         # Find 1st register and then update reg1 bits
         bin_reg1 = Assembler.REGISTERS.get(arg1)
         
-        if not bin_reg1: 
+        if bin_reg1 is None: 
             raise Assembler.NotRegisterArgumentException(None, arg1)
         
-        trans_word1 = trans_word1[:4] + bin_reg1 + trans_word1[7:]  
+        trans_word1 = trans_word1 | bin_reg1 << Assembler.REG1_OFFSET  
 
         # Find 2nd argument and then update relevant bits
         bin_reg2 = Assembler.REGISTERS.get(arg2)
         
-        if bin_reg2:
-            return trans_word1[:9] + bin_reg2 + trans_word1[12:]                
-       
+        if bin_reg2 is not None:
+            return trans_word1 | bin_reg2 << Assembler.REG2_OFFSET
         if arg2.isnumeric():
-            trans_word2 = format(int(arg2), '016b')                          
-       
+            trans_word2 = int(arg2)                          
         else:
-            trans_word2 = arg2
+            self.untranslated_exps[self.current_line_idx] = arg2
+            trans_word2 = None
         
+        self.current_address += 2
         translated_words[0] = trans_word1
-        translated_words[1] = '\n' + trans_word2 if trans_word2 != '' else trans_word2          # TODO it is terrible!
+        translated_words[1] = trans_word2
 
 
     def handle_by_opcode_type(self, words, opcode_type, translated_words):
@@ -184,7 +208,7 @@ class Assembler:
         addr_mode, clean_sec_arg = self.handle_addressing_mode(words[-1])
         
         # update addr_mode bits
-        translated_words[0] = trans_word1[:7] + addr_mode + trans_word1[9:]       
+        translated_words[0] = trans_word1 | addr_mode << Assembler.ADDR_MODE_OFFSET       
         
         # not clr inc dec jmp jne jz
         if opcode_type == 1:                                
@@ -192,9 +216,8 @@ class Assembler:
         
         # mov cmp add sub lea xor or rol
         elif opcode_type == 2:                              
-           self.handle_2_param_type(translated_words, words[1].rstrip(','), clean_sec_arg)
+            self.handle_2_param_type(translated_words, words[1].rstrip(','), clean_sec_arg)
         
-        self.current_address += 2
 
 
     def translate_line(self, asm_line):
@@ -205,12 +228,13 @@ class Assembler:
         :returns: The binary representation of the assembly line.
         :rtype: str
         """
+        self.current_line_idx += 1
         unlabeled_asm_line = self.handle_label(asm_line)
 
-        translated_words = [Assembler.EMPTY_WORD, '']
+        translated_words = [Assembler.EMPTY_WORD, None]
         # Section line check and translation
         if self.section_handler(unlabeled_asm_line, translated_words):
-            return translated_words[0]
+            return bytearray(translated_words[0])
 
         # Remove spaces from indirect expression, if any.
         words = unlabeled_asm_line.split('[')
@@ -221,7 +245,7 @@ class Assembler:
         
         # update opcode bits
         bin_opcode, opcode_type = Assembler.OPCODES.get(words[0])
-        translated_words[0] = bin_opcode + Assembler.EMPTY_WORD[4:]                   
+        translated_words[0] = Assembler.EMPTY_WORD | bin_opcode << Assembler.OPCODE_OFFSET                   
         
         if len(words[1:]) > opcode_type:
             raise Assembler.TooManyParamsException(asm_line)
@@ -232,42 +256,13 @@ class Assembler:
             e.code_line = asm_line
             raise
         
-        return ''.join(translated_words)
-
-
-    def fix_line(self, damaged_line):
+        if translated_words[1] is not None:
+            return bytearray().join([tran_word.to_bytes(2, byteorder='big') for tran_word in translated_words])
         
-        # Check if the line is not damaged
-        if damaged_line.strip('01') == '':
-            return damaged_line
-
-        # Re-write the expression as an arithmetic expression using symbol table
-        for label_tuple in self.sym_tbl:
-            damaged_line = damaged_line.replace(label_tuple, str(self.sym_tbl[label_tuple]))
-        
-        damaged_line = eval(damaged_line)
-        return format(int(damaged_line) & 0xffff, '016b')
-
-
-    def section_handler(self, asm_line, translated_words):
-        section, *rest_line = asm_line.split(maxsplit=1)
-        
-        if section == Assembler.Sections.STRING.value:
-            translated_words[0] = self.convert_str_to_binary(rest_line[0])       # remove quotes
-        elif section == Assembler.Sections.DATA.value:
-            argument = rest_line[0].lstrip()
-            if argument.isnumeric():
-                self.current_address += 2
-                translated_words[0] = format(int(argument), '016b') 
-            else:
-                raise ValueError("The input was not a number at line: " + asm_line)
-        else:
-            return False
-        return True
+        return bytearray(translated_words[0].to_bytes(2, byteorder='big'))
 
 
     def clean_comments(self, asm_lines):
-
         for index, line in enumerate(asm_lines):
             line = line.strip()
             
@@ -286,21 +281,14 @@ class Assembler:
     def first_step(self, assembly_lines):
         cleaned_lines = self.clean_comments(assembly_lines)
         semi_trans_lines = [self.translate_line(line) for line in cleaned_lines]
-        
-        # Separate adjacent lines
-        semi_trans_lines = [line.split('\n') for line in semi_trans_lines]
-        semi_trans_lines = [element for sublist in semi_trans_lines for element in sublist]
         return semi_trans_lines
 
 
     def second_step(self, semi_trans_lines):
-        # Fix lines using symbol table & place-holders
-        fixed_lines = [self.fix_line(line.strip()) for line in semi_trans_lines]
-        
-        # Prepare binary writing
-        output_binary_string = ''.join(fixed_lines)
-        bytes_string = [(output_binary_string[i:i+8]) for i in range(0, len(output_binary_string), 8)]
-        return bytes_string
+        for line_num in self.untranslated_exps:
+            eval_expr = eval(self.untranslated_exps[line_num], self.sym_tbl)
+            semi_trans_lines[line_num].extend(eval_expr.to_bytes(2, byteorder='big'))
+        return semi_trans_lines
 
     def assemble_code(self, assembly_lines):
         half_translated_lines = self.first_step(assembly_lines)
@@ -327,8 +315,8 @@ if __name__ == '__main__':
     assembled_code_string = asm.assemble_code(all_lines)
     
     with open(args.output, 'wb') as bin_out:
-        for byte_str in assembled_code_string:
-            bin_out.write(struct.pack('>B', int(byte_str, 2)))
+        for line_bytes in assembled_code_string:
+            bin_out.write(line_bytes)
 
     print("Assembling Done.")
 
