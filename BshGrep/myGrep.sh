df5ef84bfb69eb21b984bb55d16faffa5713fe97
yonits12 <yonits12@gmail.com>
2020-05-04 15:57:15 +0300

    Bug fix: when the matches substring is at index 0

--- BshGrep/myGrep.sh
+++ BshGrep/myGrep.sh
@@ -2,18 +2,18 @@
 
 RED='\033[0;31m'
 NC='\033[0m'
-
 EXPR=$1
 INPUT=$2
 
 while IFS= read -r currline; do
     if [[ $currline =~ $EXPR ]]; then
         MATCH_IDX=${currline/$BASH_REMATCH*/}
-        let REAL_LOC=${#MATCH_IDX}
-        HALF1=$(cut -c 1-${REAL_LOC} <<< $currline)
-        let AFTER_LOC=${REAL_LOC}+${#BASH_REMATCH}+1
-        HALF2=$(cut -c ${AFTER_LOC}- <<< $currline)
-        
+        HALF1=""
+        if [[ ${#MATCH_IDX} > 0 ]]; then
+            HALF1=$(cut -c 1-${#MATCH_IDX} <<< $currline)
+        fi
+        let AFTER=${#MATCH_IDX}+${#BASH_REMATCH}+1
+        HALF2=$(cut -c ${AFTER}- <<< $currline)
         echo -e "$HALF1\c"
         echo -e "$RED${BASH_REMATCH[0]}$NC\c"
         echo -e "$HALF2"
