58899b5e3546bc390669f4a3ae203d770f45d74e
yonits12 <yonits12@gmail.com>
2020-05-04 13:03:22 +0300

    Coloring works for ist instance. Input or stdin

--- BshGrep/myGrep.sh
+++ BshGrep/myGrep.sh
@@ -1,10 +1,22 @@
 #!/usr/bin/env bash
 
-INPUT=$2
+RED='\033[0;31m'
+NC='\033[0m'
+IN="Yoni ;Anna Yossi; Avner"
+sep='Anna'
 EXPR=$1
+INPUT=$2
 
 while IFS= read -r currline; do
     if [[ $currline =~ $EXPR ]]; then
-        echo "$currline"
+        END_MATCH=${currline/BASH_REMATCH[0]*/}
+        BEGIN_LINE=${currline/currline*/}
+        let REAL_LOC=${#END_MATCH}-${#BEGIN_LINE}-1
+        HALF1=$(cut -c 1-${REAL_LOC} <<< $currline)
+        let AFTER_LOC=${REAL_LOC}+${#BASH_REMATCH}+1
+        HALF2=$(cut -c ${#END_MATCH}- <<< $currline)
+        echo -e "$HALF1\c"
+        echo -e "$RED${BASH_REMATCH[0]}$NC\c"
+        echo -e "$HALF2"
     fi
-done < "$INPUT"
+done < "${INPUT:-/dev/stdin}"
