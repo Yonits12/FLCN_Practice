7410712b7caf4e88c106fba49128b1266b2830cb
yonits12 <yonits12@gmail.com>
2020-05-10 11:43:34 +0300

    Tree Final

--- BshTree/myTree.sh
+++ BshTree/myTree.sh
@@ -18,8 +18,9 @@ dive_into(){
     local curr_dir="${1}"
     local curr_prefix="${2}"
     local internal_prefix="${curr_prefix}${DEPTH}"
+    IFS=
     local list_files="${curr_dir}"/*
-    local files_array=($list_files)
+    local files_array=(${list_files})
     local files_left=${#files_array[@]}
     local branching="${BRANCH_MID}"
     for file in "$curr_dir"/*; do
@@ -29,40 +30,47 @@ dive_into(){
             internal_prefix="${internal_prefix/%"${DEPTH}"/'    '}"
         fi
         if [[ -L ${file} ]]; then #link
-            local origin="$(readlink -f ${file})"
-            if [[ "${origin}" == */*/* ]]; then
-                origin="../$(basename `dirname $origin`)/$(basename $origin)"
-            fi
+            local origin="$(readlink ${file})"
             # TODO: Handle broken links here
-            echo "${curr_prefix}${LINK}${file_name} -> ${origin}${NO_FORMAT}"
+            if [[ -d "$(readlink -f ${file})" ]]; then
+                origin="${DIR_FORMAT}${origin}${NO_FORMAT}"
+                ((++count_dirs))
+            else
+                ((++count_files))
+            fi
+            echo "${curr_prefix}${branching}${LINK}${file_name}${NO_FORMAT} -> ${origin}"
         elif [[ -d ${file} ]]; then
             echo "${curr_prefix}${branching}${DIR_FORMAT}${file_name}${NO_FORMAT}"
             dive_into "${file}" "${internal_prefix}"
+            ((++count_dirs))
         else
-            color_by_type "${file}" "${curr_prefix}${branching}"
+            ((++count_files))
+            color=$(color_by_type "${file}")
+            echo "${curr_prefix}${branching}${color}${file_name}${NO_FORMAT}"
         fi
         ((--files_left))
     done
 }
 
-# gets PATH PREFIX
+# gets PATH
 color_by_type(){
-    local path_to_file=$1
+    local path_to_file="$1"
     local filename="$(basename "${path_to_file}")"
-    local curr_prefix=$2
-    if [[ -e ${path_to_file} ]]; then   # exist
-        echo "${curr_prefix}${filename}"
-    elif [[ -f ${path_to_file} ]]; then # regular
-        echo "${curr_prefix}${LINK}${filename}${NO_FORMAT}"
+    if [[ -d ${path_to_file} ]]; then
+        echo ${DIR_FORMAT}
     elif [[ -S ${path_to_file} ]]; then
-        echo "${curr_prefix}${SOCKET}${filename}${NO_FORMAT}"
+        echo ${SOCKET}
     elif [[ -x ${path_to_file} ]]; then
-        echo "${curr_prefix}${EXE}${filename}${NO_FORMAT}"
-    else
-        echo "____${path_to_file}____ does not exist"
+        echo ${EXE}
+    elif [[ -f ${path_to_file} ]]; then
+        echo ${NO_FORMAT}
     fi
 }
 
-dir=${1:-.}
-echo "${EXE}${dir}${NO_FORMAT}"
-dive_into $dir ""
+init_dir=${1:-.}
+let count_dirs=0
+let count_files=0
+echo "${DIR_FORMAT}${init_dir}${NO_FORMAT}"
+ANCESTOR_DIRNAME="$(basename `realpath ${init_dir}`)"
+dive_into "$init_dir" ""
+echo "${count_dirs} directories, ${count_files} files"
